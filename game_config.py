f7deaae97e4ef175fbe87a4937baa6167d8f4075
abhijeet-9901 <59870936+abhijeet-9901@users.noreply.github.com>
2020-08-31 11:58:04 +0530

    Update game_config.py

--- game_config.py
+++ game_config.py
@@ -1,10 +1,10 @@
 import os
 
-IMAGE_SIZE = 128
+IMAGE_SIZE = 200
 SCREEN_SIZE = 512
 NUM_TILES_SIDE = 4
 NUM_TILES_TOTAL = 16
-MARGIN = 4
+MARGIN = 6
 
 ASSET_DIR = 'Assets'
 ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
