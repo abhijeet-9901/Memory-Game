a25ec51a8ed7c3c61a0d301f602c5a9b8d5343f8
Abhijeet Sethuraman <59870936+abhijeet-9901@users.noreply.github.com>
2020-09-01 13:53:45 +0530

    Update game.py

--- game.py
+++ game.py
@@ -34,6 +34,27 @@ running_starting_window = True
 
 screen.fill((255,255,255))
 
+while running_starting_window: # ALL THESE PROCESSES TAKES PLACE WHEN THE WINDOW IS RUNNING
+    current_event = event.get()
+    x = gs.SCREEN_SIZE // 2 - -95   # POSITION OF THE TEXT
+    y = gs.SCREEN_SIZE // 2 - -140
+    left_top_x,left_top_y,right_bottom_x,right_bottom_y = display_message('START',(255,255,255),x,y,60)
+    for e in current_event:
+        if e.type == pygame.QUIT:
+            exit()
+            running_starting_window = False
+        
+        if e.type == pygame.MOUSEBUTTONDOWN:  # THIS PROCESS IS INITIATED WHEN THE MOUSE CLICKS THE TEXT
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            display_message('START',(51,102,225),x,y,60)
+            if mouse_x >= x and mouse_y >= y and mouse_x <= x + right_bottom_x and mouse_y <= y + right_bottom_y :
+                running_starting_window = False
+    display.flip() 
+
+matched = image.load('extra/good_job.png') # GETS EXECUTED WHEN THE TILES MATCH EACHOTHER
+
+screen.fill((255,255,255))
+
 
 print ("Good Bye")
 
